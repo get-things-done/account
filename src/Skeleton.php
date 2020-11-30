b08223e2d0e3c1c64ba279ac8ebe73b20277b49b
Cao Minh Duc <caominhduc31081@gmail.com>
2020-11-30 11:02:54 +0700

    Intial Commit

---
+++ src/Skeleton.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace GetThingsDone\Skeleton;
+
+class Skeleton
+{
+}
---
+++ src/SkeletonFacade.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace GetThingsDone\Skeleton;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \GetThingsDone\Skeleton\Skeleton
+ */
+class SkeletonFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'skeleton';
+    }
+}
